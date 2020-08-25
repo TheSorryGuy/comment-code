40ef489ac9e9670f420baa3cd1a2d204101e2221
TheSorryGuy <62074121+TheSorryGuy@users.noreply.github.com>
2020-08-25 20:33:51 +0300

    Update comments.py

--- comments.py
+++ comments.py
@@ -1,27 +1,27 @@
-# ну, для начала, это, видимо, python.
-import xml.etree.ElementTree as XmlElementTree
+# ну, для начала, это, видимо, python
+import xml.etree.ElementTree as XmlElementTree # здесь импортируются модули
 import httplib2
 import uuid
 from config import ***
  
-***_HOST = '***'
+***_HOST = '***' # задаются значения переменных
 ***_PATH = '/***_xml'
 CHUNK_SIZE = 1024 ** 2
  
-def speech_to_text(filename=None, bytes=None, request_id=uuid.uuid4().hex, topic='notes', lang='ru-RU',
+def speech_to_text(filename=None, bytes=None, request_id=uuid.uuid4().hex, topic='notes', lang='ru-RU', # объявление функции, ей заданы аргументы со значениями по умолчанию
                	key=***_API_KEY):
   
-	if filename:
-    	with open(filename, 'br') as file:
-        	bytes = file.read()
+	if filename: # я не знаком с питоном, но кажется, этот кусок нужен, чтобы переводить в бинарный вид содержимое переданного файла, а так же проверять,
+    	with open(filename, 'br') as file: # передано ли функции такое значение, или данные уже в бинарном виде, и если нет, кидать исключение. если я правильно понял,
+        	bytes = file.read() # это что то вроде throw Error() в javascript
 	if not bytes:
     	raise Exception('Neither file name nor bytes provided.')
  
   
-	bytes = convert_to_pcm16b16000r(in_bytes=bytes)
+	bytes = convert_to_pcm16b16000r(in_bytes=bytes) # должно быть, это конвертация в конкретную кодировку
  
 	
-	url = ***_PATH + '?uuid=%s&key=%s&topic=%s&lang=%s' % (
+	url = ***_PATH + '?uuid=%s&key=%s&topic=%s&lang=%s' % ( # составление строки url адреса, я полагаю
     	request_id,
     	key,
     	topic,
@@ -29,10 +29,10 @@ def speech_to_text(filename=None, bytes=None, request_id=uuid.uuid4().hex, topic
 	)
  
 	
-	chunks = read_chunks(CHUNK_SIZE, bytes)
+	chunks = read_chunks(CHUNK_SIZE, bytes) # вызов функции с заданнными аргументами. судя по всему, эта функция разбивает данные на куски
  
 	
-	connection = httplib2.HTTPConnectionWithTimeout(***_HOST)
+	connection = httplib2.HTTPConnectionWithTimeout(***_HOST) # далее проставляются заголовки и по одному отправляются чанки
  
 	connection.connect()
 	connection.putrequest('POST', url)
@@ -47,23 +47,23 @@ def speech_to_text(filename=None, bytes=None, request_id=uuid.uuid4().hex, topic
     	connection.send('\r\n'.encode())
  
 	connection.send('0\r\n\r\n'.encode())
-	response = connection.getresponse()
+	response = connection.getresponse() # записывается пришедший ответ
  
 	
-	if response.code == 200:
+	if response.code == 200: # если пришел ответ ок, текст записывается в xml файл
     	response_text = response.read()
     	xml = XmlElementTree.fromstring(response_text)
  
-    	if int(xml.attrib['success']) == 1:
-        	max_confidence = - float("inf")
+    	if int(xml.attrib['success']) == 1: # если я правильно понял, то этот блок кода нужен, чтобы проверить, записан ли текст в xml файл, и обработать ошибки
+        	max_confidence = - float("inf") 
         	text = ''
  
-        	for child in xml:
-            	if float(child.attrib['confidence']) > max_confidence:
+        	for child in xml: # затрудняюсь сказать, что делает этот цикл. видимо, он перебирает элементы xml, и сравнивает значение их атрибута с переменной,
+            	if float(child.attrib['confidence']) > max_confidence: # перезаписывая её, если значение атрибута больше, а так же перезаписывая переменную текста.
                 	text = child.text
                 	max_confidence = float(child.attrib['confidence'])
  
-	        if max_confidence != - float("inf"):
+	        if max_confidence != - float("inf"): # если условие в цикле выше хотя бы раз сработало, функция возвращает текст
             	return text
         	else:
             	
@@ -75,3 +75,7 @@ def speech_to_text(filename=None, bytes=None, request_id=uuid.uuid4().hex, topic
  
 сlass SpeechException(Exception):
 	pass
+# мне не удалось быстро найти информацию, о том, что за атрибут confidence, а так же мне трудно понять, что это за странное значение: - float("inf")
+# в смысле это строка, приведенная к числу с плавающей точкой и знаком минус. подозреваю, что надо изучать питон, чтобы в этом разобраться. если это нужно
+# для каких то аспектов вакансии, я в общем готов, к тому же я решал задачки на питоне в школе и немного еще помню, однако мне странно, что для вакансии,
+# требующей знания js, вы присылаете задание на питоне.
